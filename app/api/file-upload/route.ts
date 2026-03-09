bd27c296e4df3e191a7734f442ee84c753ca8d62
BeEmpathic <xdgrzybeknr1xd@gmail.com>
2026-03-09 09:25:02 +0100

    Initial state with installed busboy and some baisic folders

---
+++ app/api/file-upload/route.ts
@@ -0,0 +1,7 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import busboy from "busboy";
+import fs from "node:fs";
+import path from "node:path";
+import { Readable } from "node:stream";
+
+function test() {}
--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,52 @@
-import Image from "next/image";
+"use client";
+import { fileUpload } from "@/_lib/file-upload/file-upload";
+import { ChangeEvent, FormEvent, useState } from "react";
 
-export default function Home() {
+export default function Page() {
+  // make it so it's just an property / object on the result
+  //  containing the messsage which is array of strings
+  // instead of this what you have now
+  const initialResult = { message: ["No files send yet"], success: true };
+
+  const [result, setResult] = useState(initialResult);
+  const [files, setFiles] = useState<File[]>([]);
+  function handleFilesChange(e: ChangeEvent<HTMLInputElement>) {
+    if (!e.target.files || e.target.files.length === 0) {
+      setFiles([]);
+      return;
+    }
+
+    setFiles(Array.from(e.target.files));
+  }
+
+  async function onSubmit(event: any) {
+    event.preventDefault();
+    const response = await fileUpload(files);
+    setResult(response);
+    console.log("The result", result);
+
+    console.log("Result message:", result.message);
+
+    console.log(files);
+  }
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <form onSubmit={onSubmit}>
+      <input type="file" multiple name="files" onChange={handleFilesChange} />
+      <button className="cursor-pointer border-solid" type="submit">
+        Submit
+      </button>
+
+      {
+        <div id="result">
+          {result.message}
+          {/* {Object.keys(result).length > 0 ? (
+            result.map((obj) => <p key={index}>{message}</p>)
+          ) : (
+            <p>No files added yet</p>
+          )} */}
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+      }
+    </form>
   );
 }
