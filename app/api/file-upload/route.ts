d63c5520014fc5fd46804d9cca2f6dc4e7972226
BeEmpathic <xdgrzybeknr1xd@gmail.com>
2026-03-10 07:53:50 +0100

    started working on the file validation

--- app/api/file-upload/route.ts
+++ app/api/file-upload/route.ts
@@ -1,7 +1,27 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
 import busboy from "busboy";
 import fs from "node:fs";
 import path from "node:path";
 import { Readable } from "node:stream";
 
-function test() {}
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export async function POST(req: NextRequest) {
+  if (!req.headers.get("content-type")?.startsWith("multipart/form-data")) {
+    return NextResponse.json({});
+  }
+
+  const bb = busboy({ headers: Object.fromEntries(req.headers) });
+
+  const savedFiles: string[] = [];
+  const validationErrors: string[] = [];
+  const fileSavePromises: Promise<void>[] = [];
+
+  bb.on("files", (fieldname, file, info) => {
+    const { filename, mimeType } = info;
+  });
+}
