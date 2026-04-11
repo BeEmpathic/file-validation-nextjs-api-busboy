816ed6da8635457ec56817f66cd3504762032d19
BeEmpathic <xdgrzybeknr1xd@gmail.com>
2026-04-11 18:26:08 +0200

    Deleted on used file-validation-frontend file it had only 10 lines of code I don't think that another file is needed for these two ifs

--- _lib/file-upload/file-validation-frontend.ts
+++
@@ -1,8 +0,0 @@
-export function checkFile(file: File) {
-  if (file.size > 5 * 1024 * 1024) {
-    return false;
-  }
-  if (!file.type.startsWith("image") || !file.type.startsWith("video")) {
-    return false;
-  }
-}
