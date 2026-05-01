efd584d83edc342b4ed4826dbcc559b1dd05cbf5
BeEmpathic <xdgrzybeknr1xd@gmail.com>
2026-05-01 16:21:43 +0200

    Created the card didn't test it yet

--- app/_components/_dropzone/DropZoneFilesPreview.tsx
+++ app/_components/_dropzone/DropZoneFilesPreview.tsx
@@ -1,1 +1,25 @@
-function dropzonePreview({ files }: { files: Array<File> }) {}
+import { useState, useEffect } from "react";
+function DropzonePreviewCard({ file }: { file: File }) {
+  const [preview, setPreview] = useState<string | Blob | undefined>("");
+  const isImage = file.type.startsWith("image/");
+
+  useEffect(() => {
+    if (!isImage) return;
+
+    const objectUrl = URL.createObjectURL(file);
+    setPreview(objectUrl);
+
+    return () => URL.revokeObjectURL(objectUrl);
+  }, [file, isImage]);
+
+  return (
+    <div className="w-100">
+      {preview ? (
+        <img src={preview} alt={file.name} className="size-full" />
+      ) : (
+        ""
+      )}
+    </div>
+  );
+}
+function DropzonePreview({ files }: { files: Array<File> }) {}
