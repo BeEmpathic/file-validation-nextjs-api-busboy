899e4b7f7739fe402d7e64820daf3acb92c7d307
BeEmpathic <xdgrzybeknr1xd@gmail.com>
2026-05-13 06:02:41 +0200

    Tryied to make the remove button's icon better but failed

--- app/_components/_dropzone/DropZoneFilesPreview.tsx
+++ app/_components/_dropzone/DropZoneFilesPreview.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { DocumentIcon } from "@heroicons/react/24/outline";
+import { DocumentIcon, XCircleIcon } from "@heroicons/react/24/outline";
 // TODOS !!!!!!!!!!!! Here comes more todos:
 // - Make remove button and SVG so it looks way better, or maybe fucking look for job instead of doing some endless project which isn't even targeted to have any users
 // - Maybe make so when you press the image remove button it changes color to red it might make it look better on the phones
@@ -51,13 +51,27 @@ const DropzonePreviewCard = ({
         {/* change this */}
         {/* File Remove button */}
         <button
-          className="text-gray-900 outline-1 outline-black hover:bg-[rgba(255,0,0,0.5)] w-[3rem] aspect-square font-bold flex items-center justify-center absolute top-1 right-1 rounded-full cursor-pointer"
+          className="hover:bg-[rgba(255,0,0,0.5)] w-[3rem] aspect-square flex items-center justify-center absolute top-1 right-1 rounded-full cursor-pointer"
           onClick={(e) => {
             e.preventDefault();
             onRemove();
           }}
         >
-          X
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="black"
+            class="size-6"
+          >
+            <path
+              stroke="white"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m11.25 9-3 3m0 0 3 3m-3-3h7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+            />
+          </svg>
         </button>
       </div>
       <div className="max-w-full bg-white p-5 text-black font-black">
