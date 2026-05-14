9414b07daa7cfbcc7e230e387bca6f3871078a7f
BeEmpathic <xdgrzybeknr1xd@gmail.com>
2026-05-14 05:19:23 +0200

    Made the remove Image button have better styling on the SVG Icon it looks pretty good

--- app/_components/_dropzone/DropZoneFilesPreview.tsx
+++ app/_components/_dropzone/DropZoneFilesPreview.tsx
@@ -1,7 +1,11 @@
 import { useState, useEffect } from "react";
-import { DocumentIcon, XCircleIcon } from "@heroicons/react/24/outline";
+import {
+  DocumentIcon,
+  XMarkIcon,
+  XCircleIcon,
+} from "@heroicons/react/24/outline";
 // TODOS !!!!!!!!!!!! Here comes more todos:
-// - Make remove button and SVG so it looks way better, or maybe fucking look for job instead of doing some endless project which isn't even targeted to have any users
+
 // - Maybe make so when you press the image remove button it changes color to red it might make it look better on the phones
 
 const DropzonePreviewCard = ({
@@ -51,27 +55,13 @@ const DropzonePreviewCard = ({
         {/* change this */}
         {/* File Remove button */}
         <button
-          className="hover:bg-[rgba(255,0,0,0.5)] w-[3rem] aspect-square flex items-center justify-center absolute top-1 right-1 rounded-full cursor-pointer"
+          className="transition-all duration-300 ease-in-out [filter:drop-shadow(0_0_1px_black)_drop-shadow(0_0_1px_black)] hover:[filter:none] hover:bg-[rgba(255,0,0,0.4)] w-[3rem] aspect-square flex items-center justify-center absolute top-1 right-1 rounded-full cursor-pointer"
           onClick={(e) => {
             e.preventDefault();
             onRemove();
           }}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="black"
-            class="size-6"
-          >
-            <path
-              stroke="white"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="m11.25 9-3 3m0 0 3 3m-3-3h7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-            />
-          </svg>
+          <XCircleIcon />
         </button>
       </div>
       <div className="max-w-full bg-white p-5 text-black font-black">
