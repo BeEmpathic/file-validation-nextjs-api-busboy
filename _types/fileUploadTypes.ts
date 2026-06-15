fa30341ba84779021de81579d711459ad60d6902
BeEmpathic <xdgrzybeknr1xd@gmail.com>
2026-06-15 05:59:42 +0200

    Deleted a comment todo

--- _types/fileUploadTypes.ts
+++ _types/fileUploadTypes.ts
@@ -9,5 +9,3 @@ export type returnedInfoType = {
   }>;
   error: string;
 };
-
-// you need to check the type returned by function to this type cause it says that you can't just return null as string check that shit what can I return
