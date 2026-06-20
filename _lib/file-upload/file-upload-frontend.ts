00e10f16d7907e0f205e03a5af6b83d7cc1cf80b
BeEmpathic <xdgrzybeknr1xd@gmail.com>
2026-06-20 14:00:19 +0200

    Fixed file size issue / bug
    It was my mistake messed up the function call by switching the proporties

--- _lib/file-upload/file-upload-frontend.ts
+++ _lib/file-upload/file-upload-frontend.ts
@@ -4,8 +4,10 @@ const FILES_MAX_AMOUNT: number = process.env.NEXT_PUBLIC_FILES_MAX_AMOUNT
   ? parseInt(process.env.NEXT_PUBLIC_FILES_MAX_AMOUNT, 10)
   : 10;
 const FILE_MAX_SIZE = process.env.NEXT_PUBLIC_FILE_MAX_SIZE
-  ? parseInt(process.env.NEXT_PUBLIC_FILE_MAX_SIZE, 10) * 1024 * 1024
+  ? parseInt(process.env.NEXT_PUBLIC_FILE_MAX_SIZE) * 1024 * 1024
   : 5 * 1024 * 1024; // in MB
+
+console.log(FILE_MAX_SIZE);
 
 const ONLY_MEDIA_ALLOWED: boolean =
   process.env.NEXT_PUBLIC_ONLY_MEDIA_ALLOWED === "true" || false;
@@ -30,7 +32,7 @@ export class FileValidationError extends Error {
 function checkFile(
   file: File,
   filesAmount: number = FILES_MAX_AMOUNT,
-  fileSizeLimit: number = FILES_MAX_AMOUNT,
+  fileSizeLimit: number = FILE_MAX_SIZE,
   onlyMedia: boolean = ONLY_MEDIA_ALLOWED,
 ):
   | {
@@ -38,10 +40,11 @@ function checkFile(
       reason: string;
     }
   | true {
+  console.log("The size of the file", file.size);
   if (file.size > fileSizeLimit) {
     return {
       fileName: file.name,
-      reason: `Is too large only up to ${fileSizeLimit / 1024 / 1024} MB allowed`,
+      reason: `Is too large only up to ${fileSizeLimit} MB allowed`,
     };
   }
   if (onlyMedia) {
@@ -74,64 +77,59 @@ export async function fileUpload(
     if (!files || files.length <= 0) {
       result.pass = false;
       result.error = "No files selected!";
-      console.log("Result when no files selected:", result);
+
       reject(result);
     }
 
-    const response = async () => {
-      for (const file of files) {
-        try {
-          const formData = new FormData();
+    for (const file of files) {
+      try {
+        const formData = new FormData();
 
-          const validation = checkFile(
-            file,
-            fileSizeLimit,
-            FILES_MAX_AMOUNT,
-            onlyMedia,
-          );
-          if (validation === true) {
-            formData.append("file", file);
-          } else {
-            result.rejectedFiles.push(validation);
-            result.error = "File didn't pass validation!";
-            result.pass = false;
-
-            reject(result);
-          }
-
-          const response = await fetch("/api/file-upload", {
-            method: "POST",
-            body: formData,
-          });
-
-          if (!response.ok) {
-            reject(result);
-          }
-          const backendData = await response.json();
-
-          result.message = backendData.message;
-          result.status = backendData.status; // this overwrites the status it's a problem
-          result.uploadedFilesNames.push(backendData.uploadedFilesNames);
-          result.rejectedFiles.push(backendData.rejectedFiles);
-          result.error = backendData.error;
-          result.pass = backendData.pass;
-
-          return response;
-        } catch (e: any) {
-          console.error("error happened: ", e);
-
-          result.error =
-            "Something went wrong! We got unusual error. Probably server";
-
+        const validation = await checkFile(
+          file,
+          FILES_MAX_AMOUNT,
+          FILE_MAX_SIZE,
+          onlyMedia,
+        );
+        if (validation === true) {
+          formData.append("file", file);
+        } else {
+          result.rejectedFiles.push(validation);
+          result.error = "File didn't pass validation!";
           result.pass = false;
+
           reject(result);
-          return response;
         }
+
+        const response = await fetch("/api/file-upload", {
+          method: "POST",
+          body: formData,
+        });
+
+        if (!response.ok) {
+          reject(result);
+        }
+        const backendData = await response.json();
+
+        result.message = backendData.message;
+        result.status = backendData.status; // this overwrites the status it's a problem
+        result.uploadedFilesNames.push(backendData.uploadedFilesNames);
+        result.rejectedFiles.push(backendData.rejectedFiles);
+        result.error = backendData.error;
+        result.pass = backendData.pass;
+      } catch (e: any) {
+        console.error("error happened: ", e);
+
+        result.error =
+          "Something went wrong! We got unusual error. Probably server";
+
+        result.pass = false;
+        reject(result);
       }
+    }
 
-      // there is type of any on error check if you can do something about it.
+    // there is type of any on error check if you can do something about it.
 
-      resolve(result);
-    };
+    resolve(result);
   });
 }
